ac3a4a8ec8525d34b7d160041147d564cd0fb32e
Mijalski <kyrcooler@gmail.com>
2021-10-05 11:55:15 +0200

    Add single person reactions analyzer

---
+++ Analyzers/ReactionsFromSinglePersonToPersonAnalyzer.cs
@@ -0,0 +1,28 @@
+﻿using MessengerDataAnalyzer.Analyzers.Generics;
+using MessengerDataAnalyzer.Models;
+
+namespace MessengerDataAnalyzer.Analyzers;
+
+public class ReactionsFromSinglePersonToPersonAnalyzer : IAnalyzer
+{
+    public bool IsDisabled { get; } = false;
+    private static readonly string PersonName = "Robert PrzyziÃ³Åkowski";
+    public string AnalyzeData(Conversation conversation)
+    {
+        var countsForConversation =
+            conversation.Messages
+                .Where(m => !m.IsUnsent && !string.IsNullOrEmpty(m.Content) && m.Reactions != null)
+                .SelectMany(m => m.Reactions, (m, r) => new { Message = m, Reaction = r})
+                .Where(mr => mr.Reaction.Actor == PersonName)
+                .GroupBy(mr => mr.Message.SenderName)
+                .Select(r => new
+                {
+                    SenderName = r.Key,
+                    ReactionCount = r.Count()
+                })
+                .OrderByDescending(m => m.ReactionCount)
+                .ToList();
+
+        return string.Join(Environment.NewLine, countsForConversation.Select(x => $"{x.SenderName};{x.ReactionCount}"));
+    }
+}
