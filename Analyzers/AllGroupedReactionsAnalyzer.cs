081b64c5b3acfefa0dcc3edf2124376028070aef
Mijalski <kyrcooler@gmail.com>
2021-10-05 14:24:25 +0200

    Add grouped reaction analyzer

---
+++ Analyzers/AllGroupedReactionsAnalyzer.cs
@@ -0,0 +1,25 @@
+﻿using MessengerDataAnalyzer.Analyzers.Generics;
+using MessengerDataAnalyzer.Models;
+
+namespace MessengerDataAnalyzer.Analyzers;
+
+public class AllGroupedReactionsAnalyzer : IAnalyzer
+{
+    public bool IsDisabled { get; } = false;
+    public string AnalyzeData(Conversation conversation)
+    {
+        var countsForConversation =
+            conversation.Messages
+                .Where(m => !m.IsUnsent && !string.IsNullOrEmpty(m.Content) && m.Reactions != null)
+                .SelectMany(m => m.Reactions)
+                .GroupBy(r => r.Reaction)
+                .Select(r => new
+                {
+                    Reaction = r.Key,
+                    ReactionCount = r.Count()
+                })
+                .ToList();
+
+        return string.Join(Environment.NewLine, countsForConversation.Select(x => $"{x.Reaction};{x.ReactionCount}"));
+    }
+}
