97fc1f337528ecbe37f890a69c5d70b5a1eabef1
Mijalski <kyrcooler@gmail.com>
2021-10-06 17:06:21 +0200

    Add word extractor

---
+++ Analyzers/WordExtractorAnalyzer.cs
@@ -0,0 +1,18 @@
+﻿using MessengerDataAnalyzer.Analyzers.Generics;
+using MessengerDataAnalyzer.Models;
+
+namespace MessengerDataAnalyzer.Analyzers;
+
+public class WordExtractorAnalyzer : IAnalyzer
+{
+    public bool IsDisabled { get; } = true;
+    public string AnalyzeData(Conversation conversation)
+    {
+        var messages =
+            conversation.Messages
+                .Where(m => !m.IsUnsent && !string.IsNullOrEmpty(m.Content))
+                .Select(m => m.Content);
+
+        return string.Join(Environment.NewLine, messages);
+    }
+}
